5cc88f3920077415d596550394ec5f86881ade7c
Matei <tools@matei.work>
2026-02-04 17:45:20 -0500

    cleanup(morphology): coasts consume beltDrivers

--- mods/mod-swooper-maps/src/recipes/standard/stages/morphology-coasts/steps/ruggedCoasts.contract.ts
+++ mods/mod-swooper-maps/src/recipes/standard/stages/morphology-coasts/steps/ruggedCoasts.contract.ts
@@ -1,7 +1,6 @@
 import { Type, defineStep } from "@swooper/mapgen-core/authoring";
 import morphology from "@mapgen/domain/morphology";
 
-import { foundationArtifacts } from "../../foundation/artifacts.js";
 import { morphologyArtifacts } from "../../morphology/artifacts.js";
 
 /**
@@ -13,7 +12,7 @@ const RuggedCoastsStepContract = defineStep({
   requires: [],
   provides: [],
   artifacts: {
-    requires: [foundationArtifacts.plates, morphologyArtifacts.topography],
+    requires: [morphologyArtifacts.beltDrivers, morphologyArtifacts.topography],
     provides: [morphologyArtifacts.coastlineMetrics],
   },
   ops: {
