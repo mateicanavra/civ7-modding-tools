24ec27a4e457051b961b670a219c07274b01a693
Matei <tools@matei.work>
2026-02-09 20:23:17 -0500

    refactor(recipe): remove ecology-features-score public/compile

--- mods/mod-swooper-maps/src/recipes/standard/stages/ecology-features-score/index.ts
+++ mods/mod-swooper-maps/src/recipes/standard/stages/ecology-features-score/index.ts
@@ -4,9 +4,5 @@ import { steps } from "./steps/index.js";
 export default createStage({
   id: "ecology-features-score",
   knobsSchema: Type.Object({}),
-  public: Type.Object({
-    scoreLayers: Type.Optional(steps.scoreLayers.contract.schema),
-  }),
-  compile: ({ config }) => ({ "score-layers": config.scoreLayers }),
   steps: [steps.scoreLayers],
 } as const);
