d9f6fe874a67cf3804f04ac2a34040cd98a3d019
Matei <tools@matei.work>
2026-02-10 15:41:31 -0500

    fix(ecology): remove biome moisture noise config
    - Delete ecology-side effectiveMoisture derivation (noise + riparian + pseudoRandom01).\n- Make biome moisture schema thresholds-only and drop humidity-driven vegetation knobs; effectiveMoisture is now Hydrology-owned.

--- mods/mod-swooper-maps/src/domain/ecology/ops/classify-biomes/rules/index.ts
+++ mods/mod-swooper-maps/src/domain/ecology/ops/classify-biomes/rules/index.ts
@@ -1,8 +1,6 @@
 export { aridityShiftForIndex, computeAridityIndex, shiftMoistureZone } from "./aridity.js";
 export { biomeSymbolForZones } from "./lookup.js";
-export { computeEffectiveMoisture, moistureZoneOf } from "./moisture.js";
-export { pseudoRandom01 } from "./noise.js";
-export { computeRiparianMoistureBonus } from "./riparian.js";
+export { moistureZoneOf } from "./moisture.js";
 export { computeTemperature, temperatureZoneOf } from "./temperature.js";
 export { clamp01, computeMaxLatitude } from "./util.js";
 export { vegetationDensityForBiome } from "./vegetation.js";
